1d3f08bbfdb27e2e9e57affb6f995b34972cd066
Jannik Lorenz <dev@janniklorenz.de>
2015-07-20 13:54:10 +0200

    add set to install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 g++ -o ./lib/HaeringAPI/Band ./lib/HaeringAPI/Band.cc
 g++ -o ./lib/HaeringAPI/NOP ./lib/HaeringAPI/NOP.cc
+g++ -o ./lib/HaeringAPI/Set ./lib/HaeringAPI/Set.cc
 npm install
 
 cp config.js.template config.js
