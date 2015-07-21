fbb10d1b9bc8101a25a54530270ed474940855af
Jannik Lorenz <dev@janniklorenz.de>
2015-07-21 18:23:48 +0200

    .

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+mkdir ./lib/HaeringAPI/bin/
 g++ -o ./lib/HaeringAPI/bin/Band ./lib/HaeringAPI/Band.cc
 g++ -o ./lib/HaeringAPI/bin/NOP ./lib/HaeringAPI/NOP.cc
 g++ -o ./lib/HaeringAPI/bin/Set ./lib/HaeringAPI/Set.cc
