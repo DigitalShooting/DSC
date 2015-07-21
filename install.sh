149be10ad3f1094ae9c5dfd015f0d92b5b5ae9d2
Jannik Lorenz <dev@janniklorenz.de>
2015-07-21 18:20:41 +0200

    .

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-g++ -o ./lib/HaeringAPI/Band ./lib/HaeringAPI/Band.cc
-g++ -o ./lib/HaeringAPI/NOP ./lib/HaeringAPI/NOP.cc
-g++ -o ./lib/HaeringAPI/Set ./lib/HaeringAPI/Set.cc
-g++ -o ./lib/HaeringAPI/ReadSettings ./lib/HaeringAPI/ReadSettings.cc
+g++ -o ./lib/HaeringAPI/bin/Band ./lib/HaeringAPI/Band.cc
+g++ -o ./lib/HaeringAPI/bin/NOP ./lib/HaeringAPI/NOP.cc
+g++ -o ./lib/HaeringAPI/bin/Set ./lib/HaeringAPI/Set.cc
+g++ -o ./lib/HaeringAPI/bin/ReadSettings ./lib/HaeringAPI/ReadSettings.cc
 #npm install
 
 cp config.js.template config.js
