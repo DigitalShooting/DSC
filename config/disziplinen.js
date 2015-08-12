372c95b5edc14a8147eeb4e6adedd3e08b044305
Jannik Lorenz <dev@janniklorenz.de>
2015-08-12 19:30:09 +0200

    .

--- config/disziplinen.js
+++ config/disziplinen.js
@@ -156,6 +156,36 @@ var scheiben = {
 		},
 		kugelDurchmesser: 4.5,
 	},
+	lpBlank: {
+		title: "LP 10m (Blank)",
+		ringe: [
+			{ value: 10, width:  11.5, color: "black", text: false, textColor: "transparent", zoom: zooms.lp.z1, hitColor: "#00bffF" },
+			{ value:  9, width:  27.5, color: "black", text: false, textColor: "transparent", zoom: zooms.lp.z1, hitColor: "#00bffF" },
+			{ value:  8, width:  43.5, color: "black", text: false, textColor: "transparent", zoom: zooms.lp.z1, hitColor: "#00bffF" },
+			{ value:  7, width:  59.5, color: "black", text: false, textColor: "transparent", zoom: zooms.lp.z1, hitColor: "#00bffF" },
+			{ value:  6, width:  75.5, color: "white", text: false, textColor: "transparent", zoom: zooms.lp.z1, hitColor: "#00bffF" },
+			{ value:  5, width:  91.5, color: "white", text: false, textColor: "transparent", zoom: zooms.lp.z1, hitColor: "#00bffF" },
+			{ value:  4, width: 107.5, color: "white", text: false, textColor: "transparent", zoom: zooms.lp.z1, hitColor: "#00bffF" },
+			{ value:  3, width: 123.5, color: "white", text: false, textColor: "transparent", zoom: zooms.lp.z1, hitColor: "#00bffF" },
+			{ value:  2, width: 139.5, color: "white", text: false, textColor: "transparent", zoom: zooms.lp.z1, hitColor: "#00bffF" },
+			{ value:  1, width: 155.5, color: "white", text: false, textColor: "transparent", zoom: zooms.lp.z1, hitColor: "#00bffF" },
+		],
+		ringeDrawOnly: [],
+		defaultZoom: zooms.lp.z1,
+		probeEcke: {
+			color: "#0f0",
+			alpha: 0.7,
+		},
+		text: {
+			size: 3.0,
+			width: 0.9,
+			up: 4.8,
+			down: -2.6,
+			left: 2.6,
+			right: -4.8,
+		},
+		kugelDurchmesser: 4.5,
+	},
 }
 
 
