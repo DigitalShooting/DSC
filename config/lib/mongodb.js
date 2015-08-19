5863655bec331aba9cbff4aeb23b1f6159887f7d
Jannik Lorenz <dev@janniklorenz.de>
2015-08-19 20:14:54 +0200

    add missing

--- config/lib/mongodb.js
+++ config/lib/mongodb.js
@@ -1,7 +1,7 @@
 // var mongodb = require('mongodb').MongoClient
 // var format = require('util').format
 // var config = require("../config/").database
-// 
+//
 // module.exports = function(callback){
 // 	mongodb.connect("mongodb://"+config.host+":"+config.port+"/"+config.collection, function (err, db) {
 // 		if (err) {
