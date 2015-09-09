064b7f44743c83591056e5e0858643470782be9f
Jannik Lorenz <dev@janniklorenz.de>
2015-09-09 16:11:11 +0200

    split

--- assets/js/services/socketio.js
+++ assets/js/services/socketio.js
@@ -8,58 +8,6 @@ angular.module('dsc.services.socketio', [
 })
 
 
-.factory('dscAPI', function (socket, auth) {
-	return {
-		setNewTarget: function(){
-			socket.emit("newTarget", {
-				auth: auth,
-			})
-		},
-		setPart: function(partId){
-			socket.emit("setPart", {
-				auth: auth,
-				partId: partId,
-			})
-		},
-		setSelectedSerie: function(index){
-			socket.emit("setSelectedSerie", {
-				auth: auth,
-				index: index,
-			})
-		},
-		setSelectedShot: function(index){
-			socket.emit("setSelectedShot", {
-				auth: auth,
-				index: index,
-			})
-		},
-		setUser: function(user){
-			socket.emit("setUser", {
-				auth: auth,
-				user: user,
-			})
-		},
-		setDisziplin: function(disziplin){
-			socket.emit("setDisziplin", {
-				auth: auth,
-				disziplin: disziplin,
-			})
-		},
-		print: function(all){
-			socket.emit("print", {
-				auth: auth,
-				all: all,
-			})
-		},
-		getTempToken: function(){
-			socket.emit("getTempToken", {
-				auth: auth,
-			})
-		},
-	}
-})
-
-
 .factory('auth', function (QueryString) {
 	return {
 		key: QueryString.key,
