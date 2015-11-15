7a296591f6588b9b5af3a8b5060045e4cfb940c2
Jannik Lorenz <dev@janniklorenz.de>
2015-11-15 13:56:10 +0100

    [ADD] SQL Sheme

---
+++ install/database.sql
@@ -0,0 +1,37 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `disziplin`;
+CREATE TABLE `disziplin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `disziplin` text COLLATE utf8_unicode_ci NOT NULL,
+  `line` text COLLATE utf8_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE `session` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `disziplinID` int(11) NOT NULL,
+  `part` text COLLATE utf8_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `shot`;
+CREATE TABLE `shot` (
+  `number` int(11) NOT NULL,
+  `sessionID` int(11) NOT NULL,
+  `ring` double NOT NULL,
+  `teiler` double NOT NULL,
+  `winkel` double NOT NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`number`,`sessionID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
