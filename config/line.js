0c242e47011e71a45b5c289a7ae35519ec962ecb
Jannik Lorenz <dev@janniklorenz.de>
2015-12-06 16:02:41 +0100

    [ADD] SQL Error Log

--- config/line.js
+++ config/line.js
@@ -5,7 +5,7 @@ module.exports = {
 	id: "stand1",
 
 	// Scale for HTML Canvas (1=2kx2k, 2=1kx1k, 4=500x500, ...)
-	scaleFactor: 4,
+	scaleFactor: 2.7,
 
 	// Host Verein (Print log)
 	hostVerein: {
