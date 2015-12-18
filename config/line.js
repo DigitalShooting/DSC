e3c969b5b8d6a5f8ed8231aaa06ba918bd628f51
Jannik Lorenz <dev@janniklorenz.de>
2015-12-18 15:41:43 +0100

    [ADD] Printer Name Config

--- config/line.js
+++ config/line.js
@@ -7,6 +7,11 @@ module.exports = {
 	// Scale for HTML Canvas (1=2kx2k, 2=1kx1k, 4=500x500, ...)
 	scaleFactor: 2.7,
 
+	printer: {
+		// Printer name in cups
+		name: "Printer1",
+	}
+
 	// Host Verein (Print log)
 	hostVerein: {
 		name: "SV Diana Dettingen",
