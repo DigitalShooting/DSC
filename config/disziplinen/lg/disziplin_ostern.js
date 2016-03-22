53a4d19ce13748553a9281cad200f6b82ae59da5
Jannik Lorenz <dev@janniklorenz.de>
2016-03-22 15:35:59 +0100

    [ADD] Ostern Disziplinen

---
+++ config/disziplinen/lg/disziplin_ostern.js
@@ -0,0 +1,56 @@
+var scheibe = require("./scheibe.js");
+
+module.exports = {
+	_id: "lg_ostern",
+	title: "LG Ostern",
+	interface: {
+		name: "demo",
+		time: 2500,
+	},
+	time: {
+		enabled: false,
+		duration: 0,
+		instantStart: false,
+	},
+	scheibe: scheibe,
+	parts: {
+		probe: {
+			title: "Probe",
+			probeEcke: true,
+			neueScheibe: false,
+			serienLength: 3,
+			anzahlShots: 3,
+			showInfos: true,
+			zehntel: false,
+			time: {
+				enabled: false,
+				duration: 0,
+				instantStart: false,
+			},
+			average: {
+				enabled: true,
+				anzahl: 5,
+			},
+			exitType: "auto",
+		},
+		match: {
+			title: "Match",
+			probeEcke: false,
+			neueScheibe: false,
+			serienLength: 5,
+			anzahlShots: 5,
+			showInfos: true,
+			zehntel: false,
+			time: {
+				enabled: false,
+				duration: 0,
+				instantStart: false,
+			},
+			average: {
+				enabled: true,
+				anzahl: 5,
+			},
+			exitType: "beforeFirst",
+		},
+	},
+};
