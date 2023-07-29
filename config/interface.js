aab157bdc9d0a7dd1b2807c7221db100dcb6cbb6
Jannik Lorenz <mail@janniklorenz.de>
2023-07-29 19:56:55 +0200

    work on reddot support

--- config/interface.js
+++ config/interface.js
@@ -22,4 +22,18 @@ module.exports = {
 		// Path to API file
 		path: "./lib/Interfaces/DeviceInterfaceDemo.js",
 	},
+
+	reddot: {
+		// Path to API file
+		path: "./lib/Interfaces/DeviceInterfaceRedDot.js",
+
+		// COM Port
+		com : "/dev/ttyUSB0",
+
+		// Band ACK
+		bandACK: {
+			enabled: false,
+			port: 61424,
+		},
+	},
 };
